01d4791e1293e8838eec4216277433a6ceb09f43
Roberto Parrella <roberto.webdeveloper@gmail.com>
2023-02-28 12:28:11 +0100

    settings

--- demo1/settings.php
+++ demo1/settings.php
@@ -1,27 +1,137 @@
 <?php
 
-    # $spBaseUrl = 'https://<your_domain>'; //or http://<your_domain>
-    $spBaseUrl = 'http://ec2-15-160-109-130.eu-south-1.compute.amazonaws.com';
+$settings = array(
+    // If 'strict' is True, then the PHP Toolkit will reject unsigned
+    // or unencrypted messages if it expects them signed or encrypted
+    // Also will reject the messages if not strictly follow the SAML
+    // standard: Destination, NameId, Conditions ... are validated too.
+    'strict' => true,
 
-    $settingsInfo = array (
-        'sp' => array (
-            'entityId' => $spBaseUrl.'/demo1/metadata.php',
-            'assertionConsumerService' => array (
-                'url' => $spBaseUrl.'/demo1/index.php?acs',
-            ),
-            'singleLogoutService' => array (
-                'url' => $spBaseUrl.'/demo1/index.php?sls',
-            ),
-            'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified',
+    // Enable debug mode (to print errors)
+    'debug' => false,
+
+    // Set a BaseURL to be used instead of try to guess
+    // the BaseURL of the view that process the SAML Message.
+    // Ex. http://sp.example.com/
+    //     http://example.com/sp/
+    'baseurl' => null,
+
+    // Service Provider Data that we are deploying
+    'sp' => array(
+        // Identifier of the SP entity  (must be a URI)
+        'entityId' => 'eni-gamindo-st',
+        // Specifies info about where and how the <AuthnResponse> message MUST be
+        // returned to the requester, in this case our SP.
+        'assertionConsumerService' => array(
+            // URL Location where the <Response> from the IdP will be returned
+            'url' => 'https://games.gamindo.com/enigoldenrules/stage',
+            // SAML protocol binding to be used when returning the <Response>
+            // message.  Onelogin Toolkit supports for this endpoint the
+            // HTTP-POST binding only
+            'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
         ),
-        'idp' => array (
-            'entityId' => 'https://sts.windows.net/c16e514b-893e-4a01-9a30-b8fef514a650/',
-            'singleSignOnService' => array (
-                'url' => 'https://login.microsoftonline.com/c16e514b-893e-4a01-9a30-b8fef514a650/saml2',
-            ),
-            'singleLogoutService' => array (
-                'url' => 'https://login.microsoftonline.com/c16e514b-893e-4a01-9a30-b8fef514a650/saml2',
-            ),
-            'x509cert' => file_get_contents(__DIR__ . '/../certs/x509.cert'),
+        // If you need to specify requested attributes, set a
+        // attributeConsumingService. nameFormat, attributeValue and
+        // friendlyName can be omitted. Otherwise remove this section.
+        "attributeConsumingService" => array(
+            "serviceName" => "SP test",
+            "serviceDescription" => "Test Service",
+            "requestedAttributes" => array(
+                array(
+                    "name" => "",
+                    "isRequired" => false,
+                    "nameFormat" => "",
+                    "friendlyName" => "",
+                    "attributeValue" => ""
+                )
+            )
         ),
-    );
+        // Specifies info about where and how the <Logout Response> message MUST be
+        // returned to the requester, in this case our SP.
+        'singleLogoutService' => array(
+            // URL Location where the <Response> from the IdP will be returned
+            'url' => 'https://login.microsoftonline.com/c16e514b-893e-4a01-9a30-b8fef514a650/saml2',
+            // SAML protocol binding to be used when returning the <Response>
+            // message.  Onelogin Toolkit supports for this endpoint the
+            // HTTP-Redirect binding only
+            'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+        ),
+        // Specifies constraints on the name identifier to be used to
+        // represent the requested subject.
+        // Take a look on lib/Saml2/Constants.php to see the NameIdFormat supported
+        'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified',
+
+        // Usually x509cert and privateKey of the SP are provided by files placed at
+        // the certs folder. But we can also provide them with the following parameters
+        'x509cert' => __DIR__ . "/../certs/sp/cert.cer",
+        'privateKey' => __DIR__ . "/../certs/sp/key.pem",
+
+        /*
+         * Key rollover
+         * If you plan to update the SP x509cert and privateKey
+         * you can define here the new x509cert and it will be
+         * published on the SP metadata so Identity Providers can
+         * read them and get ready for rollover.
+         */
+        // 'x509certNew' => '',
+    ),
+
+    // Identity Provider Data that we want connect with our SP
+    'idp' => array(
+        // Identifier of the IdP entity  (must be a URI)
+        'entityId' => 'https://sts.windows.net/c16e514b-893e-4a01-9a30-b8fef514a650/',
+        // SSO endpoint info of the IdP. (Authentication Request protocol)
+        'singleSignOnService' => array(
+            // URL Target of the IdP where the SP will send the Authentication Request Message
+            'url' => 'https://login.microsoftonline.com/c16e514b-893e-4a01-9a30-b8fef514a650/saml2',
+            // SAML protocol binding to be used when returning the <Response>
+            // message.  Onelogin Toolkit supports for this endpoint the
+            // HTTP-Redirect binding only
+            'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+        ),
+        // SLO endpoint info of the IdP.
+        'singleLogoutService' => array(
+            // URL Location of the IdP where the SP will send the SLO Request
+            'url' => '',
+            // URL location of the IdP where the SP SLO Response will be sent (ResponseLocation)
+            // if not set, url for the SLO Request will be used
+            'responseUrl' => '',
+            // SAML protocol binding to be used when returning the <Response>
+            // message.  Onelogin Toolkit supports for this endpoint the
+            // HTTP-Redirect binding only
+            'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+        ),
+        // Public x509 certificate of the IdP
+        'x509cert' => file_get_contents(__DIR__ . "/../certs/x509.cert"),
+        /*
+         *  Instead of use the whole x509cert you can use a fingerprint in
+         *  order to validate the SAMLResponse, but we don't recommend to use
+         *  that method on production since is exploitable by a collision
+         *  attack.
+         *  (openssl x509 -noout -fingerprint -in "idp.crt" to generate it,
+         *   or add for example the -sha256 , -sha384 or -sha512 parameter)
+         *
+         *  If a fingerprint is provided, then the certFingerprintAlgorithm is required in order to
+         *  let the toolkit know which Algorithm was used. Possible values: sha1, sha256, sha384 or sha512
+         *  'sha1' is the default value.
+         */
+        // 'certFingerprint' => '',
+        // 'certFingerprintAlgorithm' => 'sha1',
+
+        /* In some scenarios the IdP uses different certificates for
+         * signing/encryption, or is under key rollover phase and more
+         * than one certificate is published on IdP metadata.
+         * In order to handle that the toolkit offers that parameter.
+         * (when used, 'x509cert' and 'certFingerprint' values are
+         * ignored).
+         */
+        // 'x509certMulti' => array(
+        //      'signing' => array(
+        //          0 => '<cert1-string>',
+        //      ),
+        //      'encryption' => array(
+        //          0 => '<cert2-string>',
+        //      )
+        // ),
+    ),
+);
