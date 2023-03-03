0674e5025ef8ddf4c9a7c03221134663653e2a7a
Roberto Parrella <roberto.webdeveloper@gmail.com>
2023-03-03 11:35:39 +0100

    Update settings.php

--- eni/settings.php
+++ eni/settings.php
@@ -17,7 +17,7 @@ $settingsInfo = array(
     // the BaseURL of the view that process the SAML Message.
     // Ex. http://sp.example.com/
     //     http://example.com/sp/
-    'baseurl' => $spBaseUrl . '/' . $directory,
+    'baseurl' => $spBaseUrl,# . '/' . $directory,
 
     // Service Provider Data that we are deploying
     'sp' => array(
